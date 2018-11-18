8fa7c3880e81547be29cc4ae791ec9a5c7b0c6f6
Katie Hughes <katiehug4@gmail.com>
2018-11-17 23:55:22 -0800

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+        public IActionResult KidView()
+        {
+            ViewData["Message"] = "This is where the kid view will go.";
+
+            return View();
+        }
+
+        public IActionResult Attendance()
+        {
+            ViewData["Message"] = "Where you will keep attendance.";
+
+            return View();
+        }
+        public IActionResult Planner()
+        {
+            ViewData["Message"] = "Where you will be able to plan schedules.";
+
+            return View();
+        }
+
+        public IActionResult Badges()
+        {
+            ViewData["Message"] = "Where you will be able to assign kids badges.";
+
+            return View();
+        }
+
+        public IActionResult Stats()
+        {
+            ViewData["Message"] = "Where you will view stats about your kiddos.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
